7cb231cefd99f46a0c305f244897058c9ee2c8f0
Revelation <nusktecsoft@gmail.com>
2019-10-01 06:58:26 +0100

    Complete 72%

--- app/application/config/database.php
+++ app/application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db_local['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
@@ -93,4 +93,25 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+);
+$db['default'] = array(
+    'dsn'	=> '',
+    'hostname' => 'localhost',
+    'username' => 'benlklia_rscbyte',
+    'password' => '{c6cD]=m?szm',
+    'database' => 'benlklia_logistics',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => 'vbs_',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
